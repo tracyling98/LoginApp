238266d27a1d971de09c52839516c7f6c7ff0612
Tracy <57853991+tracyling98@users.noreply.github.com>
2021-03-19 22:46:14 +0800

    Add Registration Page

---
+++ app/src/main/java/com/example/loginapp/boundaries/RegistrationPage.java
@@ -0,0 +1,16 @@
+package com.example.loginapp.boundaries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.loginapp.R;
+
+public class RegistrationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_page);
+    }
+}
