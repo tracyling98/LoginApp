28f73da45303742b36a99e2b3865af3899f29548
Tracy <57853991+tracyling98@users.noreply.github.com>
2021-03-23 18:38:52 +0800

    Add comments

--- app/src/main/java/com/byteforce_crew/educard/boundaries/RegistrationPage.java
+++ app/src/main/java/com/byteforce_crew/educard/boundaries/RegistrationPage.java
@@ -6,11 +6,21 @@ import android.os.Bundle;
 
 import com.byteforce_crew.educard.R;
 
+// Only for moderators (organisation)
 public class RegistrationPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_page);
+
+        /* ================
+         On click listener
+        ===================*/
+
+
     }
+
+    // Methods to be used
+
 }
